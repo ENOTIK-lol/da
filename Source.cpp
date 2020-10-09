8ec34c6e8cc01fc527ea39124791edc5d8afa353
ENOTIK-lol <62998360+ENOTIK-lol@users.noreply.github.com>
2020-10-09 18:38:53 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,92 +1,106 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <time.h>
 using namespace std;
+ 
 class Perceptron
 {
 private:
-	vector<double> sm;
-	vector<double> weight;
-	double result;
-	vector<double> signals;
-	double b;
+ double result;
+ vector<double> sm;
+ vector<double> weights;
+ double b;
+ double s;
 public:
-	Perceptron(int n = 2) {
-		for (int i = 0; i < n; i++)
-		{
-			weight.push_back((rand() % 101 - 50) - 100);
-		}
-	}
-	double process(vector<double>& x) {
-		int sum = b;
-		for (int i = 0; i < x.size(); ++i) {
-			sum += x[i] * weight[i];
-		}
-		result = activationFunction(sum);
-		sm = x;
-		return result;
-	}
-	double activationFunction(double sum) {
-		return 1. / (1. + exp(-sum));
-	}
-	double dactivationFunction(double sum) {
-		return activationFunction(sum) * (1. - activationFunction(sum));
-	}
-
-	void learn(int n) {
-		double error = n - result;
-		double tetta = error * dactivationFunction(result);
-		for (int i = 0; i < weight.size(); ++i)
-		{
-			weight[i] += tetta * sm[i] * 0.3f;
-		}
-		b += tetta * 0.3f;
-
-	}
+ Perceptron(int n = 2)
+ {
+  for (int i = 0; i < n; ++i)
+  {
+   weights.push_back((rand() % 1000 - 500) / 1000.);
+  }
+  b = (rand() % 1000 - 500) / 1000.;
+ }
+ 
+ double process(vector<double>& x)
+ {
+  s = b;
+  for (int i = 0; i < x.size(); ++i)
+  {
+   s += x[i] * weights[i];
+  }
+  result = activationFunction(s);
+  sm = x;
+  return result;
+ }
+ 
+ double activationFunction(double aS)
+ {
+  return 1. / (1. + exp(-aS));
+ }
+ 
+ double dActivationFunction(double aS)
+ {
+  return activationFunction(aS) * (1 - activationFunction(aS));
+ }
+ 
+ void learn(double n)
+ {
+  double error = n - result;
+  double tetta = error * dActivationFunction(s);
+  for (int i = 0; i < weights.size(); ++i)
+  {
+   weights[i] += tetta * sm[i] * 0.3f;
+  }
+  b += tetta;
+ }
 };
+void getAnswer(int a, int b, vector<double>& answer)
+{
+ int result = a + b;
+ answer[result] = 0.5f;
+ for (int i = result - 1; i >= 0; --i)
+ {
+  answer[i] = answer[i + 1] - 0.5f / answer.size();
+ }
+ for (int i = result + 1; i < answer.size(); ++i)
+ {
+  answer[i] = answer[i - 1] + 0.5f / answer.size();
+ }
+}
 int main()
 {
-
-	vector<double> answ;
-	vector<double> a = { 50, 28 };
-	vector<Perceptron> perceptrons;
-	for (int i = 0; i < 500; ++i)
-	{
-		answ.push_back(0);
-		perceptrons.push_back(Perceptron(2));
-	}
-	answ[a[0] + a[1]] = 0.5f;
-	for (int i = a[0] + a[1] - 1; i > 0; --i)
-	{
-		answ[i] = answ[i + 1] - 0.5f / 500;
-		cout << answ[i] << ' ';
-	}
-	cout << endl;
-	for (int i = a[0] + a[1] + 1; i < 500; ++i)
-	{
-		answ[i] = answ[i - 1] + 0.5f / 500;
-		cout << answ[i] << ' ';
-	}
-	int right = 0;
-	int neright = 0;
-	while (true)
-	{
-		for (double i = 0; i < 250; ++i)
-		{
-
-			for (double j = 0; j < 250; ++j)
-			{
-				vector<double> temp = { i,j };
-				for (int k = 0; k < 500; ++k)
-				{
-					double answ_l = perceptrons[k].process(temp);
-					if (abs(0.5f - answ_l) == 0.5f) right++;
-					else neright++;
-					cout << right << ' ';
-					cout << neright << endl;
-					perceptrons[k].learn(answ[k]);
-				}
-			}
-		}
-	}
+ srand(time(0));
+ vector<Perceptron> perceptrons(200, Perceptron(2));
+ int agr = 0;
+ int disagr = 0;
+ vector<double> answer(200);
+ while (true)
+ {
+  for (double i = 0; i < 100; i++)
+  {
+   for (double j = 0; j < 100; j++)
+   {
+    vector<double> temp_l = { i, j };
+    
+    getAnswer(i, j, answer);
+    int winner = -1;
+    float minimum = 1.;
+    for (int k = 0; k < 200; k++)
+    {
+     double temp = perceptrons[k].process(temp_l);
+     if (abs(temp - 0.5f) < minimum)
+     {
+      minimum = abs(temp - 0.5f);
+      winner = k;
+     }
+     perceptrons[k].learn(answer[k]);
+    }
+    if (winner == i + j) agr++;
+    else disagr++;
+   }
+  }
+  cout << agr << ' ' << disagr << endl;
+  agr = 0;
+  disagr = 0;
+ }
 }
